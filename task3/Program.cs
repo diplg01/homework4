415a9dd001ac79b9d9888584ac3ae806e0f0c07e
diplg <dimas4354@icloud.com>
2022-04-29 01:37:56 +0300

    задание 3

---
+++ task3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int []mas = new int [8];
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = new Random().Next();
+    Console.WriteLine($"{mas[i]} ");
+}
